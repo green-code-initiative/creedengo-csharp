59f88cd662ae117438015e451ec786e592364d0f
Vianney de Bellabre <Djoums@users.noreply.github.com>
2025-01-07 13:12:18 +0100

    Switch to creedengo-C# (#88)

---
+++ src/Creedengo.Tests/Tests/GCI86.GCCollectShouldNotBeCalled.Tests.cs
@@ -0,0 +1,118 @@
+﻿namespace Creedengo.Tests.Tests;
+
+[TestClass]
+public class GCCollectShouldNotBeCalledTests
+{
+    private static readonly AnalyzerDlg VerifyAsync = TestRunner.VerifyAsync<GCCollectShouldNotBeCalled>;
+
+    [TestMethod]
+    public Task EmptyCodeAsync() => VerifyAsync("");
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledAsync() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                [|GC.Collect()|];
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledSystemAsync() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                [|System.GC.Collect()|];
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledNamedArgumentsAsync() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                [|GC.Collect(mode: GCCollectionMode.Optimized, generation: 1)|];
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledMultipleCodeAsync() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                string text="";       [|GC.Collect()|]; string text2 = "";
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledCommentedAsync() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                //GC.Collect();
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledGeneration0Async() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                GC.Collect(0);
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledGeneration10Async() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                [|GC.Collect(10)|];
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledGeneratio0CollectionModeAsync() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                GC.Collect(0, GCCollectionMode.Forced);
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task GCCollectShouldNotBeCalledGeneration10CollectionModeAsync() => VerifyAsync("""
+        using System;
+        public static class Program
+        {
+            public static async void Main()
+            {
+                [|GC.Collect(10, GCCollectionMode.Forced)|];
+            }
+        }
+        """);
+}
---
+++ src/Creedengo.Tool/Services/ReportService.cs
@@ -0,0 +1,19 @@
+﻿namespace Creedengo.Tool.Services;
+
+internal static partial class ReportService
+{
+    public static async Task GenerateReportAsync(List<DiagnosticInfo> diagnostics, string outputPath, OutputType outputType)
+    {
+        var writer = new StreamWriter(outputPath, append: false);
+        await using (writer.ConfigureAwait(false)) // In two steps to avoid CA2008 Consider calling ConfigureAwait on the awaited task
+        {
+            switch (outputType)
+            {
+                case OutputType.Html: await Html.WriteToStreamAsync(writer, diagnostics).ConfigureAwait(false); break;
+                case OutputType.Json: await Json.WriteToStreamAsync(writer, diagnostics).ConfigureAwait(false); break;
+                case OutputType.Csv: await Csv.WriteToStreamAsync(writer, diagnostics).ConfigureAwait(false); break;
+                default: throw new NotSupportedException();
+            }
+        }
+    }
+}
