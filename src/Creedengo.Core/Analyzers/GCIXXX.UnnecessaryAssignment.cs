3d851c4bc232961c55e336b3c6726a15605ca436
Jonathan Bihet <jonathan.bihet@hagergroup.com>
2025-05-20 14:58:12 +0200

    first naive implem

---
+++ src/Creedengo.Core/Analyzers/GCIXXX.UnnecessaryAssignment.cs
@@ -0,0 +1,68 @@
+﻿namespace Creedengo.Core.Analyzers;
+
+/// <summary>GCIXXX: Unnecessary assignment.</summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class UnecessaryAssignment : DiagnosticAnalyzer
+{
+    private static readonly ImmutableArray<SyntaxKind> SyntaxKinds = [SyntaxKind.SimpleAssignmentExpression, SyntaxKind.AddAssignmentExpression];
+
+    /// <summary>The diagnostic descriptor.</summary>
+    public static DiagnosticDescriptor Descriptor { get; } = Rule.CreateDescriptor(
+        id: Rule.Ids.GCIXX_UnecessaryAssignment,
+        title: "Unnecessary assignment",
+        message: "Assignment is not necessary.",
+        category: Rule.Categories.Performance,
+        severity: DiagnosticSeverity.Info,
+        description: "Assignments shoult not be made when not necessary.");
+
+    /// <inheritdoc/>
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => _supportedDiagnostics;
+    private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics = [Descriptor];
+
+    /// <inheritdoc/>
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.RegisterSyntaxNodeAction(static context => AnalyzeInvocationExpression(context), SyntaxKinds);
+    }
+
+    private static void AnalyzeInvocationExpression(SyntaxNodeAnalysisContext context)
+    {
+        var assignmentExpr = (AssignmentExpressionSyntax)context.Node;
+
+        if (assignmentExpr.Left is not IdentifierNameSyntax identifierName ||
+            context.SemanticModel.GetSymbolInfo(assignmentExpr.Left).Symbol is not ILocalSymbol localSymbol ||
+            localSymbol.DeclaringSyntaxReferences.Length != 1)
+        {
+            return;
+        }
+
+        var methodDeclaration = assignmentExpr.FirstAncestorOrSelf<MethodDeclarationSyntax>();
+        if (methodDeclaration == null)
+        {
+            return;
+        }
+
+        var assignments = methodDeclaration.DescendantNodes()
+            .OfType<AssignmentExpressionSyntax>()
+            .Where(a => context.SemanticModel.GetSymbolInfo(a.Left).Symbol?.Equals(localSymbol) == true)
+            .ToList();
+
+        if (assignments.Count > 1 && assignments.Where(a => context.SemanticModel.GetConstantValue(a.Right).HasValue).Count() > 1)
+        {
+            //for (int i = 0; i < assignments.Count; i++)
+            //{
+            //    var currentAssignment = assignments[i];
+            //    //var previousAssignment = assignments[i - 1];
+
+            //    if (context.SemanticModel.GetConstantValue(currentAssignment.Right).HasValue && )
+            //    {
+            //        context.ReportDiagnostic(Diagnostic.Create(Descriptor, currentAssignment.GetLocation()));
+            //    }
+            //}
+
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, assignmentExpr.GetLocation()));
+        }
+    }
+}
---
+++ src/Creedengo.Tests/Tests/GCIXXX.UnnecessaryAssignment.Tests.cs
@@ -0,0 +1,126 @@
+﻿namespace Creedengo.Tests.Tests;
+
+[TestClass]
+public sealed class UnnecessaryAssignmentTests
+{
+    //private static readonly CodeFixerDlg VerifyAsync = TestRunner.VerifyAsync<UnecessaryAssignment, UnnecessaryAssignmentFixer>;
+    private static readonly AnalyzerDlg VerifyAsync = TestRunner.VerifyAsync<UnecessaryAssignment>;
+
+    [TestMethod]
+    public Task EmptyCodeAsync() => VerifyAsync("");
+
+    //[TestMethod]
+    //public Task NominalCase() => VerifyAsync("""
+    //    class TestClass
+    //    {
+    //        int TestMethod()
+    //        {
+    //            if (condition)
+    //            {
+    //                x = 1;
+    //            }
+    //            else
+    //            {
+    //                x = 2;
+    //            }
+
+    //            return x;
+    //        }
+    //    }
+    //    """, """
+    //    class TestClass
+    //    {
+    //        int TestMethod()
+    //        {
+    //            if (condition)
+    //            {
+    //                return 1;
+    //            }
+    //            else
+    //            {
+    //                return 2;
+    //            }
+    //        }
+    //    }
+    //    """);
+
+    [TestMethod]
+    public Task NominalCase() => VerifyAsync("""
+        class TestClass
+        {
+            int TestMethod(bool condition)
+            {
+                int x;
+                if (condition)
+                {
+                    [|x = 1|];
+                }
+                else
+                {
+                    [|x = 2|];
+                }
+
+                return x;
+            }
+        }
+        """);
+
+
+    [TestMethod]
+    public Task NominalCase2() => VerifyAsync("""
+        class TestClass
+        {
+            int TestMethod(bool condition)
+            {
+                int x;
+                if (condition)
+                {
+                    [|x = 1|];
+                }
+                else
+                {
+                    [|x = 2|];
+                }
+
+                [|x += 3|];
+                return x;
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task NominalCase3() => VerifyAsync("""
+        class TestClass
+        {
+            int TestMethod(bool condition)
+            {
+                int x;
+                
+                [|x = 1|];
+                
+                [|x = 2|];
+                [|x = 3|];
+                [|x = 4|];
+        
+                return x;
+            }
+        }
+        """);
+
+    [TestMethod]
+    public Task NominalCase4() => VerifyAsync("""
+        class TestClass
+        {
+            (int, int) TestMethod(bool condition)
+            {
+                var x = (10, 20);
+                
+                [|x = (15, 30)|];
+                
+                [|x = (20, 21)|];
+                
+                return x;
+            }
+        }
+        """);
+}
